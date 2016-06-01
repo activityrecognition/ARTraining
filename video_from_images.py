4ae75f29efb5f3d4b5bfae67d0796935966ffda3
fpolacov <fpolacov@gmail.com>
2016-06-01 12:23:43 -0300

    subvideos in compossed video sorted

--- video_from_images.py
+++ video_from_images.py
@@ -1,26 +1,31 @@
 #!/usr/bin/env python
 import requests, os, sys, getopt, ast, PIL, av, numpy
 from PIL import Image
+from operator import itemgetter
 
+FILE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+<<<<<<< Updated upstream
 FILE_DIR = os.path.abspath(os.path.dirname(__file__))
 #FILE_DIR = os.path.abspath(os.path.dirname("."))
 
+=======
+>>>>>>> Stashed changes
 defaut_work_dir = "dataset/4_tim"
 default_file_name = "thermix_1_testing.txt"
 default_output_dir = "mov"
-
-classes = ["1", "3", "4"]
-files = {}
-
+default_classes = ["1", "3", "4"]
 
 def main():
-    # create the files structure
-    for c in classes:
-        files.update({c: []})
-
     wd = defaut_work_dir
     fn = default_file_name
     od = default_output_dir
+    classes = default_classes
+
+    # create the files structure
+    files = {}
+    for c in classes:
+        files.update({c: []})
 
     # save frames according to class
     base_dir_path = os.path.join(FILE_DIR, wd)
@@ -49,10 +54,30 @@ def main():
         os.makedirs(output_dir)
 
     for c in files.keys():
+        print "build video for class index %s" % c
         class_paths = files[c]
-        class_paths = sorted(class_paths)
 
+<<<<<<< Updated upstream
         output_path = os.path.join(output_dir, prefix_output+"_"+c+".mov")
+=======
+        # Sort video path
+        videos_path = {}
+        for file in class_paths:
+            if not videos_path.get(os.path.dirname(file), None):
+                videos_path[os.path.dirname(file)] = []
+            name, extension = os.path.basename(file).split(".")
+            videos_path[os.path.dirname(file)].append((int(name), extension))
+
+        class_paths = []
+        for video in videos_path.keys():
+            video_frames = sorted(videos_path[video], key=itemgetter(0))
+            for v in video_frames:
+                class_paths.append(os.path.join(video, "%d.%s" % v))
+        # END Sort
+
+        # make video for class
+        output_path = os.path.join(output_dir, c+".mov")
+>>>>>>> Stashed changes
         output = av.open(output_path, 'w')
         stream = output.add_stream("mpeg4", "10")
 
